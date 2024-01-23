b561fcce59003136a6cb0bf0e8d0f2a872cda4ce
Neha Rajesh <leehanseok3@gmail.com>
2024-01-23 05:58:46 +0000

    refactor: move components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { Header } from "./Header";
-import { Footer } from "./Footer";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 
 export default function Home() {
   return (
