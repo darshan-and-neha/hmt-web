8e1e24e4ff3c40a6a612327dd34cfabab228a3e4
Neha Rajesh <leehanseok3@gmail.com>
2024-01-31 06:02:19 +0000

    update: color palette

--- src/theme.ts
+++ src/theme.ts
@@ -11,6 +11,36 @@ const roboto = Roboto({
 const theme = createTheme({
   palette: {
     mode: "light",
+    primary: {
+      light: "#F4F1DE",
+      main: "#EAB69F",
+      dark: "#E07A5F",
+    },
+    secondary: {
+      light: "#F5F1ED",
+      main: "#D4D3D5",
+      dark: "#B3B5BD",
+    },
+    success: {
+      light: "#C8E6C9",
+      main: "#A5D6A7",
+      dark: "#66BB6A",
+    },
+    error: {
+      light: "#FFCDD2",
+      main: "#EF9A9A",
+      dark: "#EF5350",
+    },
+    info: {
+      light: "#B3E5FC",
+      main: "#81D4FA",
+      dark: "#29B6F6",
+    },
+    warning: {
+      light: "#FFE0B2",
+      main: "#FFCC80",
+      dark: "#FFA726",
+    },
   },
   typography: {
     fontFamily: roboto.style.fontFamily,
