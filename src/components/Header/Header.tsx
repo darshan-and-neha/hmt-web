97f832a0ffd92afb1f18e4d19a14a8004b4b26bc
Neha Rajesh <leehanseok3@gmail.com>
2024-02-21 06:46:54 +0000

    update: add images

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,22 @@
 import styles from "./header.module.css";
 import { Button } from "@mui/material";
+import Image from "next/image";
 
 export const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <a className={styles.logoText} href="/">
-          Hire My Talent
+        <a href="/">
+          <Image
+            src={"/assets/hmt-cropped-nobg.png"}
+            alt="hmt logo image"
+            width={90}
+            height={50}
+          />
         </a>
+        {/* <a className={styles.logoText} href="/">
+          Hire My Talent
+        </a> */}
       </div>
       <Button
         className={styles.loginButton}
